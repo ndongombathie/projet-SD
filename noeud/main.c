e095958ac144e6789134b88eb6aad46e01c59d8b
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-04-03 10:00:50 +0000

    les noeuds version final

--- noeud/main.c
+++ noeud/main.c
@@ -9,15 +9,19 @@ int main()
     a=inserer_droite(a,5,7);  //on insere un nouveau noeud a la droite de 5 avec la valeur 7
     a=inserer_gauche(a,15,12); //on insere un nouveau noeud a la gauche de 15 avec la valeur 12
     a=inserer_droite(a,15,20);  //on insere un nouveau noeud a la droite de 15 avec la valeur 20
+    printf("\n parcours prefixe: ");
     parcours_prefixe(a); //on parcourt l'arbre en prefixe
-    printf("\n");
+    printf("\n parcours infixe: ");
     parcours_infixe(a); //on parcourt l'arbre en infixe
-    printf("\n");
+    printf("\n parcours postfixe: ");
     parcours_postfixe(a); //on parcourt l'arbre en postfixe
-    printf("\n");
-    afficher_arbre(a,0); //on affiche l'arbre
     si_est_dans_arbre(a,7);  //on verifie si 7 est dans l'arbre
     si_est_dans_arbre(a,12);  //on verifie si 12 est dans l'arbre
     si_est_dans_arbre(a,25);  //on verifie si 25 est dans l'arbre
+    a=inserer_gauche(a,10,8); //on insere un nouveau noeud a la gauche de 10 avec la valeur 8
+    a=inserer_droite(a,20,25);  //Insérer le nœud 25 comme fils droit du nœud 20
+    printf("hauteur de l'arbre: %d\n",hauteur(a)); //on calcule la hauteur de l'arbre
+    printf("\n affichage de l'arbre: \n");
+    afficher_arbre(a,0); //on affiche l'arbre
     return 0;
 }
